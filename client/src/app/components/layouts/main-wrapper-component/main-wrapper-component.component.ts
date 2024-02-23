50f49df3eb1b764612768b8ab7fdf39d8a6d526a
Dmytrii9Cherniak <dcheyrnak10@gmail.com>
2024-02-23 20:23:03 +0200

    added first routing project structure

---
+++ client/src/app/components/layouts/main-wrapper-component/main-wrapper-component.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-wrapper-component',
+  templateUrl: './main-wrapper-component.component.html',
+  styleUrls: ['./main-wrapper-component.component.scss']
+})
+export class MainWrapperComponentComponent {}
